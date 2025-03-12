5cccbeaf3711c30bd6b96a2d797f0bd45c8243b0
Lorenz Kupfer <lorenz.kupfer@4sellers.de>
2025-03-12 08:49:37 +0100

    Implemented Knight

--- 4Chess/Pieces/Knight.cs
+++ 4Chess/Pieces/Knight.cs
@@ -19,61 +19,65 @@ namespace _4Chess.Pieces
 
         public override List<(int, int)> GetMoves()
         {
+            List<(int, int)> moves = [];
+
             //oben links
             if(X-1 >= 0 && Y-2 >= 0)
             {
                 if (TempGame.Board[Y - 2][X - 1] == null || TempGame.Board[Y - 2][X - 1]?.Alignment != this.Alignment)
-                    PossibleMoves.Add((Y - 2, X - 1));
+                    moves.Add((Y - 2, X - 1));
             }
 
             //oben rechts
             if (X + 1 < TempGame.Board.Count && Y - 2 >= 0)
             {
                 if (TempGame.Board[Y - 2][X + 1] == null || TempGame.Board[Y - 2][X + 1]?.Alignment != this.Alignment)
-                    PossibleMoves.Add((Y - 2, X + 1));
+                    moves.Add((Y - 2, X + 1));
             }
 
             //rechts oben
             if (X + 2 < TempGame.Board.Count && Y - 1 >= 0)
             {
                 if (TempGame.Board[Y - 1][X + 2] == null || TempGame.Board[Y - 1][X + 2]?.Alignment != this.Alignment)
-                    PossibleMoves.Add((Y - 1, X + 2));
+                    moves.Add((Y - 1, X + 2));
             }
 
             //rechts unten
             if (X + 2 < TempGame.Board.Count && Y + 1 < TempGame.Board.Count)
             {
                 if (TempGame.Board[Y + 1][X + 2] == null || TempGame.Board[Y + 1][X + 2]?.Alignment != this.Alignment)
-                    PossibleMoves.Add((Y + 1, X + 2));
+                    moves.Add((Y + 1, X + 2));
             }
 
             //unten rechts
             if (X + 1 < TempGame.Board.Count && Y + 2 < TempGame.Board.Count)
             {
                 if (TempGame.Board[Y + 2][X + 1] == null || TempGame.Board[Y + 2][X + 1]?.Alignment != this.Alignment)
-                    PossibleMoves.Add((Y + 2, X + 1));
+                    moves.Add((Y + 2, X + 1));
             }
 
             //unten links
             if (X - 1 >= 0 && Y + 2 < TempGame.Board.Count)
             {
                 if (TempGame.Board[Y + 2][X - 1] == null || TempGame.Board[Y + 2][X - 1]?.Alignment != this.Alignment)
-                    PossibleMoves.Add((Y + 2, X - 1));
+                    moves.Add((Y + 2, X - 1));
             }
 
             //links unten
             if (X - 2 >= 0 && Y + 1 < TempGame.Board.Count)
             {
                 if (TempGame.Board[Y + 1][X - 2] == null || TempGame.Board[Y + 1][X - 2]?.Alignment != this.Alignment)
-                    PossibleMoves.Add((Y + 1, X - 2));
+                    moves.Add((Y + 1, X - 2));
             }
 
             //links oben
             if (X - 2 >= 0 && Y - 1 >= 0)
             {
                 if (TempGame.Board[Y - 1][X - 2] == null || TempGame.Board[Y - 1][X - 2]?.Alignment != this.Alignment)
-                    PossibleMoves.Add((Y - 1, X - 2));
+                    moves.Add((Y - 1, X - 2));
             }
+
+            return moves;
         }
     }
 }
