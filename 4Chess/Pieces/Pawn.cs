2b569c29058673e207d5e86963ea5b11b62a1855
TristanAtze <ben.sowieja@4sellers.de>
2025-03-12 13:18:11 +0100

    4passant

--- 4Chess/Pieces/Pawn.cs
+++ 4Chess/Pieces/Pawn.cs
@@ -5,6 +5,7 @@ namespace _4Chess.Pieces
     class Pawn : Piece
     {
         public bool IsUnmoved { get; set; } = true;
+        public bool IsEnPassant { get; set; } = false;
 
         public Pawn(int yPosition, int xPosition, Color alignment)
         {
@@ -31,21 +32,27 @@ namespace _4Chess.Pieces
 
             moves.Add(new Vector2(X, Y + yDiff));
 
-            if (IsUnmoved) moves.Add(new Vector2(X, Y + yDiff * 2));
-
-            if(X - 1 >= 0)
+            if (IsUnmoved)
             {
-                if (TempGame.Board[Y + yDiff][X - 1]?.Alignment != this.Alignment)
+                moves.Add(new Vector2(X, Y + yDiff * 2));
+            }
+
+            if (X - 1 >= 0)
+            {
+                if (TempGame.Board[Y + yDiff][X - 1]?.Alignment != this.Alignment ||
+                    (TempGame.Board[Y][X - 1] is Pawn leftPawn && leftPawn.Alignment != this.Alignment && leftPawn.IsEnPassant))
                     moves.Add(new Vector2(X - 1, Y + yDiff));
             }
 
-            if (X + 1 >= 0)
+            if (X + 1 < TempGame.Board.Count)
             {
-                if (TempGame.Board[Y + yDiff][X + 1]?.Alignment != this.Alignment)
+                if (TempGame.Board[Y + yDiff][X + 1]?.Alignment != this.Alignment ||
+                     (TempGame.Board[Y][X - 1] is Pawn rightPawn && rightPawn.Alignment != this.Alignment && rightPawn.IsEnPassant))
                     moves.Add(new Vector2(X + 1, Y + yDiff));
             }
 
             //en passant implementieren
+            //Bool in Move Methode, dass der Bauer sich 2 Felder bewegt hat
 
             ValidateMoves();
 
